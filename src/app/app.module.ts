55cede4df0e3cd50033c51e37bbf1f77e8979ac1
Gvaldz <233415@ids.upchiapas.edu.mx>
2024-11-05 12:21:09 -0600

    add: Se crean los componentes tablero y cuadro y se les da estilos temporales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CuadroComponent } from './cuadro/cuadro.component';
+import { TableroComponent } from './tablero/tablero.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuadroComponent,
+    TableroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cuadro/cuadro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cuadro',
+  templateUrl: './cuadro.component.html',
+  styleUrl: './cuadro.component.css'
+})
+export class CuadroComponent {
+
+}
